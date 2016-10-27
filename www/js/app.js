65cbf44d9568e394305faeb6d65e110746895c2e
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-10-27 23:11:02 +0530

    Add configuration provider for API url

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,10 @@
 // Ionic Starter App
 
 angular.module('TaggerMobile', ['ionic'])
-.config(function($stateProvider,$urlRouterProvider){
+.config(function($stateProvider,$urlRouterProvider, configProvider){
+
+  configProvider.locals.baseApiUrl = "http://ec2-54-186-114-41.us-west-2.compute.amazonaws.com:3000/";
+
   $stateProvider
   .state('login',{
     url:'/login',
