27431d1b3aa1e856b003d2e8f28455c05dd33cb9
MaduraHerath <maduraherath8@gmail.com>
2016-11-11 02:25:03 +0530

    Add Search view

--- www/js/controllers/DashController.js
+++ www/js/controllers/DashController.js
@@ -1,9 +1,12 @@
 angular.module('TaggerMobile')
-.controller('DashCtrl', function($scope, RetailService) {
+.controller('DashCtrl', function($state,$scope, RetailService) {
   	console.log("In Dash Controller");
 
   	$scope.recent = [];
-
+    $scope.gotoSearchView = function(){
+      $state.go('search-view');
+      console.log("Switching to search view")
+    }
   	$scope.refreshRecentList = function(){
   		RetailService.getRecentProducts().then(function(result){
   			$scope.recent = result;
