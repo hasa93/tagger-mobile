bc198453d05a9fd64d9ff6eb15f9ed9e1bd5bc51
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-11-14 22:56:18 +0530

    Add NFC tag listener

--- www/js/services/NfcService.js
+++ www/js/services/NfcService.js
@@ -4,8 +4,15 @@ angular.module('TaggerMobile')
 
 	$ionicPlatform.ready(function(){
 		console.log("Standing by for NFC...");
+
+		nfc.addTagDiscoveredListener(function(event){
+			console.log(event);
+		}, function(success){
+			console.log("Listening for tags...");
+		})
+
 		nfc.showSettings(function(devInfo){
-			console.log(devInfo);
+			console.log(JSON.stringify(devInfo));
 		}, function(err){
 			console.log(err);
 		})
